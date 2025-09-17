5c9232a6cf068fa1764874881dddde5b9f81f958
JackMorin25 <morjm-25@rhodes.edu>
2025-09-17 09:23:26 -0500

    database setup and connection in app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import db
+import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'backend.sqlite'),
+)
 
 
 @app.route("/")
 def hello_world():
+    db.init_db()
     return "<p>Hello, World!</p>"
 
 app.run(host="0.0.0.0", port=80)
