3549749486139757faeb974f581206f7dc05c0cc
JackMorin25 <morjm-25@rhodes.edu>
2025-09-18 21:01:42 -0500

    same

--- backend/services/memoService.py
+++ backend/services/memoService.py
@@ -3,8 +3,9 @@ import json
 
 class MemoPuller:
 
-    def __init__(self, db_filename="database.db"):
+    def __init__(self):
         self.db_url = f"host=http://127.0.0.1:80"
 
     def getMemo():
+        #get info about stored cards
         pass
