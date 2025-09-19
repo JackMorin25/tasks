6963e8f414f78ed01d5c5a326b03dc71b4221335
JackMorin25 <morjm-25@rhodes.edu>
2025-09-18 21:01:33 -0500

    backend stuff

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,6 @@
 import sqlite3
 from datetime import datetime
 
-import click
 from flask import current_app, g
 
 
---
+++ backend/services/memoService.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+class MemoPuller:
+
+    def __init__(self, db_filename="database.db"):
+        self.db_url = f"host=http://127.0.0.1:80"
+
+    def getMemo():
+        pass
