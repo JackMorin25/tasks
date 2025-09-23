366d53d64f217d908a116f66634a8529c6e2157e
JackMorin25 <morjm-25@rhodes.edu>
2025-09-22 22:51:16 -0500

    added spaces between inputs

--- frontend/src/components/popup/popup.jsx
+++ frontend/src/components/popup/popup.jsx
@@ -3,12 +3,11 @@ const Popup = () => {
     return (
         <div id="form-container">
             <form>
-                <label for="title">Title:</label>
-                <input type="text" id="title" name="title"></input>
-                <label for="msg-content">Text:</label>
+                <label for="title">Title:</label><br/>
+                <input type="text" id="title" name="title"></input><br/>
+                <label for="msg-content">Text:</label><br/>
                 <input type="text" id="msg-content" name="title"></input>
             </form>
-
         </div>
     )
 }
