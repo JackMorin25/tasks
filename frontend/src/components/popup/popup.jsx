d5f266db06d4de0ba669147a11837766b6f8a589
JackMorin25 <morjm-25@rhodes.edu>
2025-09-22 22:44:29 -0500

    popup html base

--- frontend/src/components/popup/popup.jsx
+++ frontend/src/components/popup/popup.jsx
@@ -1,8 +1,14 @@
 const Popup = () => {
 
     return (
-        <div>
-            
+        <div id="form-container">
+            <form>
+                <label for="title">Title:</label>
+                <input type="text" id="title" name="title"></input>
+                <label for="msg-content">Text:</label>
+                <input type="text" id="msg-content" name="title"></input>
+            </form>
+
         </div>
     )
 }
