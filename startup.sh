2b4b85c79e87039115a860ab382100ea09651ca4
JackMorin25 <morjm-25@rhodes.edu>
2025-09-26 20:49:27 -0500

    startup script works with docker

--- startup.sh
+++ startup.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 
-# Store the PIDs of our processes
 backend_pid=""
 frontend_pid=""
 
-# Function to kill processes on script exit
 cleanup() {
   echo "Shutting down servers..."
   [ ! -z "$backend_pid" ] && kill $backend_pid
   [ ! -z "$frontend_pid" ] && kill $frontend_pid
-  
-  
-  
+  docker stop memo
   exit
 }
 
-# Set trap to ensure cleanup happens when script is terminated
-trap cleanup INT TERM EXIT
+trap cleanup INT TERM
 
+# Start Docker container
+echo "Starting Docker container..."
+docker start memo
 
-# Navigate to the backend directory and start the backend server
-cd ./backend
-echo "Activating virtual environment..."
-source ./.venv/bin/activate   # <<--- activate your venv
-echo "Starting backend server..."
-echo "Starting backend server..."
-python app.py &
-backend_pid=$!
-echo "Backend PID: $backend_pid"
+# Start backend
+#cd ./backend
+#echo "Starting backend server..."
+#python app.py &
+#backend_pid=$!
+#echo "Backend PID: $backend_pid"
 
-# Navigate to the frontend directory and start the frontend server
-cd ../frontend
+# Start frontend
+cd ./frontend
 echo "Starting frontend server..."
 npm run dev &
 frontend_pid=$!
 echo "Frontend PID: $frontend_pid"
 
-echo "All services running. Press Ctrl+C to stop all servers."
+echo "All services running. Press Ctrl+C to stop."
 
-# Wait for all background jobs to finish
 wait
